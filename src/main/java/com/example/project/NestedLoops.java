f43c3b00a9246ede650a69a2f4d8443af1fef189
Hunter Chen <hunterc23@nycstudents.net>
2025-12-10 18:28:44 +0000

    e

--- src/main/java/com/example/project/NestedLoops.java
+++ src/main/java/com/example/project/NestedLoops.java
@@ -17,7 +17,14 @@ return the string below for given height=5
 *****  
            */
 public static String starStaircase(int height){
-    return "";
+    String result = "";
+    for (int i = 1; i<=height; i++) {
+        for (int o = 1; o<=i; o++) {
+            result += "*";
+        }
+        result += "\n";
+    }
+     return result.substring(0, result.length() - 1);
 }
           /*
 return the string below for given height=5
@@ -29,7 +36,15 @@ return the string below for given height=5
            */
 
 public static String starStaircaseReverse(int height){
-    return "";
+
+    String result = "";
+    for (int i = height; i>0; i--) {
+        for (int o = 1; o<=i; o++) {
+            result += "*";
+        }
+        result += "\n";
+    }
+     return result.substring(0, result.length() - 1);
 }
             /*
 
@@ -41,8 +56,32 @@ width=5 height=5
 *****
           */
     public static String emptyBox(int width, int height) {
-        return "";
+
+        if (width == 1) {
+            return "";
+        }
+        String b = "";
+        while (b.length()<width && width > 1) {
+            b += "*" ;
+        }
+        String result = b + "\n";
+        
+    for (int i = height-2; i>0; i--) {
+        for (int o = 1; o<=width; o++) {
+
+            if (o == 1 || o == width) {
+            result += "*";
+            } else {
+            result += " ";
+            }
     }
+        result += "\n";
+    }
+
+    result += b;
+     return result;
+}
+
                     /*
 return the string below for word="HELLO" rows=5                    
 HELLO
@@ -54,7 +93,20 @@ OHELL
   i will provide a hint for this one if you get stuck.. use modulo
                 */
     public static String repeatRectangle(String word, int rows){
-        return "";
-    }
+        String result = "";
+        String c= "";
+        for (int i = 0; i<=rows; i++) {
+            for (int e= i%word.length(); e <word.length(); e++){
+                c += word.charAt(e);
+            }
 
+            for (int r= 0; r <i%word.length(); r++){
+                c += word.charAt(r);
+            }
+            result+=c;
+            result += "\n";
+            c = "";
+        }
+        return result.substring(0, result.length() - 1);
+    }
 }
